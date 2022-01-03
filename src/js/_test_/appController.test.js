99ca31e7e17eff0e64b874fa25546d1d3ae669e5
Appveyor <ci@appveyor.com>
2022-01-03 17:46:35 +0300

    f

--- src/js/_test_/appController.test.js
+++ src/js/_test_/appController.test.js
@@ -3,8 +3,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
+
 const config = require('../webpack.dev');
-const server = new WebpackDevServer(webpack(config), {});
+const server = new WebpackDevServer(webpack(config),{});
 server.listen(9000, 'localhost', (err) => {
   if (err) {
     return;
@@ -13,5 +14,3 @@ server.listen(9000, 'localhost', (err) => {
     process.send('ok');
   }
 });
-
-
